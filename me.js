029ebff8ca216a05f36239497f56ad6b1ffc00a6
aindrajaya <arista.indrajaya@gmail.com>
2020-03-23 17:04:12 +0700

    init

---
+++ me.js
@@ -0,0 +1,132 @@
+// utilities
+var get = function (selector, scope) {
+  scope = scope ? scope : document;
+  return scope.querySelector(selector);
+};
+
+var getAll = function (selector, scope) {
+  scope = scope ? scope : document;
+  return scope.querySelectorAll(selector);
+};
+
+// setup typewriter effect in the terminal demo
+if (document.getElementsByClassName('demo').length > 0) {
+  var i = 0;
+  var txt = `i'm Arista Indrajaya
+            I'm a full-stack software and blockchain developer based out of Depok, Indonesia, and I create informative, accesible, and fast website. I prever leave every bit of code I touch more performant and accessible development.
+
+            Apart from full-stack and blockchain coding laborer, I'm also passionate about listening and sharing what I've learned both with my teammates and the local or broader web and blockchain community. I've gained so much from others sharing their skills and knowledge, and willing to not only perform web development work but also collaborate and helps other so we can improve our work, skill, and team capability.
+
+            I'm trying to tend to help bring out a cleared sense of purpose and encourage clearer team communication, resulting in better team results and a higher sense of satisfaction for all involved.
+            
+            - think
+            - learn
+            - code
+            - test 🥝`;
+  var speed = 25;
+
+  function typeItOut () {
+    if (i < txt.length) {
+      document.getElementsByClassName('demo')[0].innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeItOut, speed);
+    }
+  }
+
+  setTimeout(typeItOut, 1800);
+}
+
+// toggle tabs on codeblock
+window.addEventListener("load", function() {
+  // get all tab_containers in the document
+  var tabContainers = getAll(".tab__container");
+
+  // bind click event to each tab container
+  for (var i = 0; i < tabContainers.length; i++) {
+    get('.tab__menu', tabContainers[i]).addEventListener("click", tabClick);
+  }
+
+  // each click event is scoped to the tab_container
+  function tabClick (event) {
+    var scope = event.currentTarget.parentNode;
+    var clickedTab = event.target;
+    var tabs = getAll('.tab', scope);
+    var panes = getAll('.tab__pane', scope);
+    var activePane = get(`.${clickedTab.getAttribute('data-tab')}`, scope);
+
+    // remove all active tab classes
+    for (var i = 0; i < tabs.length; i++) {
+      tabs[i].classList.remove('active');
+    }
+
+    // remove all active pane classes
+    for (var i = 0; i < panes.length; i++) {
+      panes[i].classList.remove('active');
+    }
+
+    // apply active classes on desired tab and pane
+    clickedTab.classList.add('active');
+    activePane.classList.add('active');
+  }
+});
+
+//in page scrolling for documentaiton page
+var btns = getAll('.js-btn');
+var sections = getAll('.js-section');
+
+function setActiveLink(event) {
+  // remove all active tab classes
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].classList.remove('selected');
+  }
+
+  event.target.classList.add('selected');
+}
+
+function smoothScrollTo(i, event) {
+  var element = sections[i];
+  setActiveLink(event);
+
+  window.scrollTo({
+    'behavior': 'smooth',
+    'top': element.offsetTop - 20,
+    'left': 0
+  });
+}
+
+if (btns.length && sections.length > 0) {
+  for (var i = 0; i<btns.length; i++) {
+    btns[i].addEventListener('click', smoothScrollTo.bind(this,i));
+  }
+}
+
+// fix menu to page-top once user starts scrolling
+window.addEventListener('scroll', function () {
+  var docNav = get('.doc__nav > ul');
+
+  if( docNav) {
+    if (window.pageYOffset > 63) {
+      docNav.classList.add('fixed');
+    } else {
+      docNav.classList.remove('fixed');
+    }
+  }
+});
+
+// responsive navigation
+var topNav = get('.menu');
+var icon = get('.toggle');
+
+window.addEventListener('load', function(){
+  function showNav() {
+    if (topNav.className === 'menu') {
+      topNav.className += ' responsive';
+      icon.className += ' open';
+    } else {
+      topNav.className = 'menu';
+      icon.classList.remove('open');
+    }
+  }
+  icon.addEventListener('click', showNav);
+});
+
---
+++ repocard.js
@@ -0,0 +1,98 @@
+let cards = document.getElementsByClassName('gh-card');
+
+function injectStyle(str) {
+    let node = document.createElement('style');
+    node.innerHTML = str;
+    document.body.appendChild(node);
+}
+
+function injectStylesheet(url) {
+  let node = document.createElement('link');
+  node.setAttribute('rel', 'stylesheet');
+  node.setAttribute('href', url);
+  document.body.appendChild(node);
+}
+
+let style = `
+.gh h4 {
+  padding: 0;
+  margin: 0;
+}
+
+.gh a, .gh a:visited {
+  text-decoration: none;
+  color: black;
+}
+
+.gh-small {
+  zoom: 0.5;
+}
+
+.gh-medium {
+  zoom: 0.75;
+}
+
+.gh-large {
+  zoom: 1;
+}
+.gh-card {
+    font-family: 'Arial';
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    max-width: 300px;
+    display: inline-block;
+  margin: 8px;
+  border-radius: 5px;
+}
+
+.gh-card:hover {
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+img.gh {
+  border-radius: 5px 5px 0 0;
+  width: 100%;
+clip-path: polygon(100% 0, 100% 95%, 50% 100%, 0% 95%, 0 0);
+}
+
+.container.gh {
+  padding: 16px;
+}
+
+.gh p {
+  line-height: 1.6;
+}
+`;
+
+injectStylesheet('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+injectStyle(style);
+
+for(let card of cards) {
+  let repo = card.getAttribute('repo');
+  let url = 'https://api.github.com/repos/' + repo;
+  
+  fetch(url, {method: 'GET'}).then(resp => {
+    return resp.json();
+  }).then(json => {
+    // <img class="gh" src="http://lorempixel.com/400/200"> masuk card.innerHTML
+    card.innerHTML = `
+      <div class="gh container">
+        <h4 class="gh">
+          <a class="gh" href="${json.html_url}">
+            <i class="fa fa-fw fa-github" aria-hidden="true"></i>
+            ${json.full_name}
+          </a>
+        </h4>
+        <p class="gh">${json.description}</p>
+        <a class="gh" href="${json.html_url}/network">
+          <i class="fa fa-fw fa-code-fork" aria-hidden="true"></i> ${json.forks_count}
+        <a class="gh" href="${json.html_url}/stargazers">
+          <i class="fa fa-fw fa-star" aria-hidden="true"></i> ${json.stargazers_count}
+        </a>
+      </div>
+    `;
+    
+  }).catch(err => {
+    console.log(err);
+  });
+}
