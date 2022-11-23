ca0e1f6dec5f005637619d1361740b6a5908b482
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-11-24 06:49:00 +0700

    add content and name

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,21 @@
+import { Container, Box, Heading } from "@chakra-ui/react";
+
 const Page = () => {
   return (
-    <div>Helloooossss</div>
+    <Container>
+      <Box borderRadius="lg" bg="green" p={3} mb={6} align="center">
+        Hi, I&apos;m Website Apps Developer based in Indonesia
+      </Box>
+
+      <Box display={{md: "flex"}}>
+        <Box>
+          <Heading as="h2" variant="page-title">
+            Arista Indrajaya
+          </Heading>
+          <p>Website Apps Developer (Web3 / React / JavaScript)</p>
+        </Box>
+      </Box>
+    </Container>
   )
 }
 
