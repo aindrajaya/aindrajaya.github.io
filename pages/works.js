73747bf861a3b6d6d946a7d93e8950e5aab7a60e
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-11-28 05:24:00 +0700

    add works page to show the recent work

---
+++ pages/works.js
@@ -0,0 +1,14 @@
+import { Container, Heading, SimpleGrid, Divider } from "@chakra-ui/react";
+import Section from "../components/section";
+
+const Works = () => {
+  return(
+    <Container>
+      <Heading as="h3" fontSize={20} mb={4}>
+        Works
+      </Heading>
+    </Container>
+  )
+}
+
+export default Works
