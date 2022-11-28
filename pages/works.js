26293ab4f5ccf9f952eabe4f00e7555aa3d705ff
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-11-29 05:14:00 +0700

    Update works page, adding section

--- pages/works.js
+++ pages/works.js
@@ -9,10 +9,13 @@ import dappCrowdfundingThumb from "../public/images/works/dappcrowdfunding.jpg"
 const Works = () => {
   return(
     <Container>
-      <Heading as="h3" fontSize={20} mb={4}>
+      <Heading as="h3" fontSize={20} mb={6}>
         Works
       </Heading>
-
+      <Divider />
+      <Heading as="h4" fontSize={16} mt={2} mb={4}>
+        Personal Works
+      </Heading>
       <SimpleGrid columns={[1, 1, 2]} gap={6}>
         <Section>
           <WorkGridItem id="react-map" title="React Map" thumbnail={reactMapThumb}>
@@ -24,11 +27,17 @@ const Works = () => {
           A decentralized crowdfunding app (also known as a web3 crowdfunding app) is a DApp that enables users to participate in crowdfunding campaigns using cryptocurrency or other digital assets.
           </WorkGridItem>
         </Section>
-
-        {/* With Delay 0.1 */}
-
-        {/* With Delay 0.2 */}
       </SimpleGrid>
+      <Divider />
+      <Heading as="h4" fontSize={16} mt={2} mb={4}>
+        Collaboration Works
+      </Heading>
+      {/* With Delay 0.1 */}
+      <Divider />
+      <Heading as="h4" fontSize={16} mt={2} mb={4}>
+        Non-profit Works
+      </Heading>
+      {/* With Delay 0.2 */}
     </Container>
   )
 }
