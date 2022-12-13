cb813bf59ebef8efd5fcd0caa1f9e0a191104ff3
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-12-14 05:00:00 +0700

    create work layout

--- components/work.js
+++ components/work.js
@@ -1,8 +1,8 @@
 import NextLink from "next/link";
-import { Heading, Box, Image, Link, Badge } from "@chakra-ui/react";
+import { Heading, Box, Image, Link, Badge, useColorModeValue } from "@chakra-ui/react";
 import { ChevronRightIcon } from "@chakra-ui/icons";
 
-export const Title = ({children}) => {
+export const Title = ({children}) => (
   <Box>
     <Link as={NextLink} href="/works">
       Works
@@ -15,14 +15,17 @@ export const Title = ({children}) => {
       {children}
     </Heading>
   </Box>
-}
+)
 
-export const WorkImage = ({src, alt}) => {
+export const WorkImage = ({src, alt}) => (
   <Image borderRadius="lg" w="full" src={src} alt={alt} mb={4}/>
-}
+)
 
-export const Meta = ({}) => {
-  <Badge colorScheme="green" mr={2}>
-    {children}
-  </Badge>
+export const Meta = ({children}) => {
+  const color = useColorModeValue('yellow', 'green')
+  return (
+    <Badge colorScheme={color} mr={2}>
+      {children}
+    </Badge>
+  )
 }
