d4c90915ca4e90ec69388f607bbae18ac943cdf2
Arista Indrajaya <arista.indrajaya@gmail.com>
2023-04-30 13:39:25 +0700

    create youtube player component

--- components/work.js
+++ components/work.js
@@ -22,6 +22,21 @@ export const WorkImage = ({src, alt}) => (
   <Image borderRadius="lg" w="full" src={src} alt={alt} mb={4}/>
 )
 
+export const YoutubeVideo = ({videoId}) => {
+  return (
+    <LazyLoad>
+      <iframe
+        width={560}
+        height={315}
+        src={`https://www.youtube.com/embed/${videoId}`}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </LazyLoad>
+  )
+}
+
 export const Meta = ({children}) => {
   const color = useColorModeValue('yellow', 'green')
   return (
